63fff3873dc322ac5bea169cab7a9a4c5a915e3b
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-16 18:49:55 +0900

    Title: 소수점 반올림하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250116/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double a = 25.352;
+        System.out.println("%.1f",a);
+    }
+}
