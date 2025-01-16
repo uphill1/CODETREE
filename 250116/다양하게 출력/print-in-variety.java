a3890d3dfe1a8c6880e4d054e3f35cdb187fe97e
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-16 18:29:40 +0900

    Title: 다양하게 출력, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

--- 250116/다양하게 출력/print-in-variety.java
+++ 250116/다양하게 출력/print-in-variety.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.println("Toral days in Year");
+        System.out.println("Total days in Year");
         System.out.println(365);
         System.out.println("Circumference rate");
         System.out.println(3.1415926535);
