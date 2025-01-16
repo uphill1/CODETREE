e291b559ef335b3d8b8479651c8746b5f8537604
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-17 00:13:55 +0900

    Title: 변수 값 교체하기 6, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250117/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 2 , b=5,temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
