df4abb5358af90d8e30f33e22530e2a436ddd87d
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-16 17:24:46 +0900

    Title: 단어 출력, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 250116/단어 출력/print-word.java
+++ 250116/단어 출력/print-word.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.print("Hello")
+        System.out.print("Hello");
     }
 }
