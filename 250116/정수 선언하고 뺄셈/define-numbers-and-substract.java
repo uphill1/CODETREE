7e0132c154d7517422046639c3c5484f6065fbdc
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-16 18:33:26 +0900

    Title: 정수 선언하고 뺄셈, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250116/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 250116/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -3,7 +3,7 @@ public class Main {
         // Please write your code here.
         int a=97;
         int b=13;
-        System.out.print("97-13=");
+        System.out.print("97 - 13 = ");
         System.out.print(a+b);
     }
 }
