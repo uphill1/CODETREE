b88b428ba58959771ce2f2bfe0ad28aa48732397
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-16 17:53:14 +0900

    Title: 숫자 2개 출력, Time: 81ms, Memory: 9MB, Status: Passed - CodeTree

--- 250116/숫자 2개 출력/print-two-numbers.java
+++ 250116/숫자 2개 출력/print-two-numbers.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        System.out.print("3 + " " + 5");
+        System.out.print(3 + " " + 5);
         
 
 
