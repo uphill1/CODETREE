15755ecc04ab245457afa01d3ac2dbfe0eec38e0
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-01-17 00:37:27 +0900

    Title: 점수 입출력, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250117/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.printf("Your score is %d point.",n);
+    }
+}
