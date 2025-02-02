4106350093983f9dc78473348940d3a3f57385d5
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-02-02 22:21:08 +0900

    Title: 입력받아 연산하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250202/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        System.out.printf("%d\n",a+87);
+        System.out.printf("%d\n",b%10);
+
+
+    }
+
+}
