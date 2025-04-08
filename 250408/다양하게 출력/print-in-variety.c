f2eab7f7726e1ce6c1c7f81deea6dd30e89317bf
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-04-08 22:28:48 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250408/다양하게 출력/print-in-variety.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    printf("Total days in Year\n");
+    printf("365\n");
+    printf("Circumference rate\n");
+    printf("3.1415926535");
+
+    return 0;
+}
