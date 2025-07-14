fce19570044d6f48e1eb3219f6b9a70bb582adb0
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-07-14 14:03:14 +0900

    Title: 다양하게 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250714/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "Total days in Year\n";
+    cout << 365 <<endl;
+    cout << "Circumference rate\n";
+    cout << "3.1415926535"; // 그냥 숫자만 넣으니까 소수점 6째에서 막히네
+    return 0;
+}
