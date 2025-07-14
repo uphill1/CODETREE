2587af35f8c25e69eb4478d844829920f7ee260d
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-07-14 21:32:04 +0900

    Title: 변수 값 복사하기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250714/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a =1;
+    int b =5;
+    int c =3;
+    a=c;
+    a=a+c;
+    b=b-c;
+    cout << a<<endl<<b<<endl<<c;
+    return 0;
+}
