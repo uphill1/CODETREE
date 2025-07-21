cc2f78586fe9978b11abbd0443f8e780255fc6a7
uphill1 <123743906+uphill1@users.noreply.github.com>
2025-07-21 19:25:18 +0900

    Title: 방향에 맞춰 이동, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250721/방향에 맞춰 이동/move-in-direction.py
+++ 250721/방향에 맞춰 이동/move-in-direction.py
@@ -1,11 +1,11 @@
 n = int(input())
-moves = [tuple(input().split()) for _ in range(n)]
 x, y = 0,0
 dx = [1,-1,0,0]
 dy= [0,0,-1,1]
 
 for i in range(n):
-    dirc, dist = tuple(input().split)
+    dirc, dist = tuple(input().split())
+    dist = int(dist)
 
     if dirc == "E":
         move=0
